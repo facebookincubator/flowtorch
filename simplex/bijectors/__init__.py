e82628c3227c4e02f5b97fe6840d94c26544a3a7
Stefan Webb <info@stefanwebb.me>
2020-12-14 21:09:12 -0800

    simplex.bijectors.Sigmoid (untested)

--- simplex/bijectors/__init__.py
+++ simplex/bijectors/__init__.py
@@ -4,8 +4,10 @@
 # TODO: Scan all classes deriving from Bijector in simplex.bijector and add here automatically
 from simplex.bijectors.affine_autoregressive import AffineAutoregressive
 from simplex.bijectors.compose import Compose
+from simplex.bijectors.sigmoid import Sigmoid
 
 __all__ = [
     "AffineAutoregressive",
-    "Compose"
+    "Compose",
+    "Sigmoid"
 ]
--- simplex/utils.py
+++ simplex/utils.py
@@ -1,6 +1,12 @@
 # Copyright (c) Simplex Development Team. All Rights Reserved
 # SPDX-License-Identifier: MIT
 
+import torch
+
 def clamp_preserve_gradients(x, min, max):
     # This helper function clamps gradients but still passes through the gradient in clamped regions
     return x + (x.clamp(min, max) - x).detach()
+
+def clipped_sigmoid(x):
+    finfo = torch.finfo(x.dtype)
+    return torch.clamp(torch.sigmoid(x), min=finfo.tiny, max=1. - finfo.eps)
