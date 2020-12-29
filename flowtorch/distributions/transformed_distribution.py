Fix sign error in TransformedDistribution.log_prob (#14)
Fixes #12

Co-authored-by: Stefan Webb <info@stefanwebb.me>

flowtorch/distributions/transformed_distribution.py
@@ -62,7 +62,7 @@ class TransformedDistribution(torch.distributions.Distribution):
         event_dim = len(self.event_shape)
 
         x = self.bijector.inverse(y, self.params)
-        log_prob = _sum_rightmost(
+        log_prob = -_sum_rightmost(
             self.bijector.log_abs_det_jacobian(x, y, self.params),
             event_dim - self.bijector.event_dim,
         )