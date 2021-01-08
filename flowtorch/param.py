663abfab47a8654c1d8c8292f0257147dd898c0e
Stefan Webb <info@stefanwebb.me>
2021-01-07 20:26:05 -0800

    Type hints to `param.py`

--- flowtorch/param.py
+++ flowtorch/param.py
@@ -1,6 +1,25 @@
 # Copyright (c) FlowTorch Development Team. All Rights Reserved
 # SPDX-License-Identifier: MIT
+from typing import Dict, Optional, Sequence, Tuple, Union
+
 import torch
+import torch.nn as nn
+
+
+class ParamsModule(torch.nn.Module):
+    def __init__(
+        self, params: "Params", modules: nn.ModuleList, buffers: Dict[str, torch.Tensor]
+    ):
+        super(ParamsModule, self).__init__()
+        self.params = params
+        self.mods = modules
+
+        if buffers is not None:
+            for n, v in buffers.items():
+                self.register_buffer(n, v)
+
+    def forward(self, x):
+        return self.params.forward(x, modules=self.mods)
 
 
 class Params(object):
@@ -10,44 +29,41 @@ class Params(object):
 
     def __init__(self):
         super(Params, self).__init__()
-        # self._inv = None
 
-    def __call__(self, input_shape, param_shapes):
-        # TODO: Take this class out of Params!
-        class ParamsModule(torch.nn.Module):
-            def __init__(self, params, modules, buffers):
-                super(ParamsModule, self).__init__()
-                self.params = params
-                self.mods = modules
-
-                # DEBUG
-                # for m in modules:
-                #    print(m)
-
-                if buffers is not None:
-                    for n, v in buffers.items():
-                        self.register_buffer(n, v)
-
-            def forward(self, x):
-                return self.params.forward(x, modules=self.mods)
-
+    def __call__(
+        self, input_shape: torch.Size, param_shapes: Sequence[torch.Size]
+    ) -> ParamsModule:
         return ParamsModule(self, *self.build(input_shape, param_shapes))
 
-    def forward(self, x, context=None, modules=None):
+    def forward(
+        self,
+        x: torch.Tensor,
+        context: Optional[torch.Tensor] = None,
+        modules: Optional[nn.ModuleList] = None,
+    ) -> Optional[Union[torch.Tensor, Sequence[torch.Tensor]]]:
         return self._forward(x, context=context, modules=modules)
 
-    def _forward(self, x, context=None, modules=None):
+    def _forward(
+        self,
+        x,
+        context: Optional[torch.Tensor] = None,
+        modules: Optional[nn.ModuleList] = None,
+    ) -> Optional[Union[torch.Tensor, Sequence[torch.Tensor]]]:
         """
         Abstract method to ***
         """
         raise NotImplementedError
 
-    def build(self, input_shape, param_shapes):
+    def build(
+        self, input_shape: torch.Size, param_shapes: Sequence[torch.Size]
+    ) -> Tuple[nn.ModuleList, Dict[str, torch.Tensor]]:
         self.input_shape = input_shape
         self.param_shapes = param_shapes
         return self._build(input_shape, param_shapes)
 
-    def _build(self, input_shape, param_shapes):
+    def _build(
+        self, input_shape: torch.Size, param_shapes: Sequence[torch.Size]
+    ) -> Tuple[nn.ModuleList, Dict[str, torch.Tensor]]:
         """
         Abstract method to ***
         """
