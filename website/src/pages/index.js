3b9e79bfc0779c49c18398fc791e175096794e22
Stefan Webb <info@stefanwebb.me>
2021-01-21 11:47:55 -0800

    Working on CSS of front page

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -5,12 +5,6 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import styles from './styles.module.css';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
-
-library.add(fab, faCheckSquare, faCoffee)
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const features = [
   {
@@ -80,7 +74,7 @@ function Home() {
                 styles.getStarted,
               )}
               to={useBaseUrl('users/')}>
-              <FontAwesomeIcon icon="coffee" /> Get Started
+              Get Started
             </Link>
             </div>
           </div>
