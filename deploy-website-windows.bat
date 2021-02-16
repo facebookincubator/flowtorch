31b1fdf8042af0ccc4731a25cf5f9eb33b3d11af
Stefan Webb <info@stefanwebb.me>
2021-02-15 16:45:54 -0800

    Need batch file in both master and website branches

--- deploy-website-windows.bat
+++ deploy-website-windows.bat
@@ -1,17 +1,16 @@
-cd ..
 call git checkout master
-rmdir /q /s .website
+rmdir /q /s ./website/build
 cd website
 call yarn install
 call yarn build
-move ./build ../.website
+REM move ./build ../.website
 cd ..
 call git checkout website
 call git clean -f -d
 call git rm -r .
-xcopy /E /I .website ./
+xcopy /E /I ./website/build ./
 call git add .
 call git commit -a -m "Updating website"
 call git push
 call git checkout master
-rmdir /q /s .website
+rmdir /q /s ./website/build
