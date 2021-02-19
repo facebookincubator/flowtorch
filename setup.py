bbb1e6da694ad38f09d139a51435532f35032b09
Stefan Webb <info@stefanwebb.me>
2021-02-18 17:04:23 -0800

    setup.py

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ setup(
         "relative_to": __file__,
         "write_to": os.path.join("flowtorch", "version.py"),
     },
-    packages=find_packages(include=["flowtorch"]),
+    packages=find_packages(include=["flowtorch", "flowtorch.*"]),
     extras_require={
         "dev": DEV_REQUIRES,
         "test": TEST_REQUIRES,
