90cd20d8c14b471bb2cc183bcd0a45dd76255626
Stefan Webb <info@stefanwebb.me>
2021-04-18 12:10:50 -0700

    Moved experimental initialization of `DenseAutoregressive` to its own module

---
+++ flowtorch/experimental/params/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) FlowTorch Development Team. All Rights Reserved
+# SPDX-License-Identifier: MIT
+
+from flowtorch.experimental.params.dense_autoregressive import DenseAutoregressive
+
+__all__ = [
+    "DenseAutoregressive",
+]
