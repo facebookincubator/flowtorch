8f5e70a8c761728ec3f7ca7ba2f08a558ec803ce
Stefan Webb <info@stefanwebb.me>
2021-10-25 14:29:31 -0700

    Changed `samples` to `examples` and wrapped in `if __name__`

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ setup(
     },
     packages=find_packages(
         include=["flowtorch", "flowtorch.*"],
-        exclude=["debug", "tests", "website", "samples", "scripts"],
+        exclude=["debug", "tests", "website", "examples", "scripts"],
     ),
     extras_require={
         "dev": DEV_REQUIRES,
