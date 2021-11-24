031ab4d776bf3a7a07394813932821461b47914b
Stefan Webb <info@stefanwebb.me>
2021-11-23 19:20:50 -0800

    Integrating API generation script with repo

--- scripts/generate_api_docs.py
+++ scripts/generate_api_docs.py
@@ -16,65 +16,276 @@ any working directory.
 
 """
 
-import errno
+import pkgutil
+import importlib
+import inspect
+from inspect import ismodule, isclass, isfunction, signature
+import re
+import toml
 import os
+import errno
+from typing import Any, Tuple
 
-import flowtorch
-from flowtorch.docs import (
-    documentable_modules,
-    generate_markdown,
-    module_hierarchy,
-    name_entity_mapping,
-)
+from docs import sparse_module_hierarchy, walk_packages, generate_class_markdown, generate_module_markdown, generate_function_markdown
 
-if __name__ == "__main__":
-    # Create website/docs/api if doesn't exist
-    try:
-        os.makedirs(os.path.join(flowtorch.__path__[0], "../website/docs/api"))
-    except OSError as e:
-        if e.errno != errno.EEXIST:
-            raise
-
-    # Build sidebar JSON based on module hierarchy and save to 'website/api.sidebar.js'
-    all_sidebar_items = []
-
-    documentable_module_names = {m.__name__: v for m, v in documentable_modules.items()}
-
-    def module_sidebar(mod_name, items):
-        return f"{{\n  type: 'category',\n  label: '{mod_name}',\n  \
-collapsed: {'false' if mod_name in module_hierarchy.keys() else 'true'},\
+def module_sidebar(mod_name, items):
+    return f"{{\n  type: 'category',\n  label: '{mod_name}',\n  \
+collapsed: {'true'},\
   items: [{', '.join(items)}],\n}}"
 
-    def dfs(dict):
-        sidebar_items = []
-        for key, val in dict.items():
+def fullname(key, item):
+    return key + '.' + item
+
+def dfs(dict):
+    sidebar_items = []
+    for key, val in dict.items():
+        if len(modules_and_symbols[key][1]) > 0:
             items = (
-                [f'"api/{key}"']
-                + [f'"api/{key}.{item[0]}"' for item in documentable_module_names[key]]
-                if len(documentable_module_names[key]) > 0
-                else []
+                [f'"api/{symbol_to_article[key]}"']
+                + [f'"api/{symbol_to_article[fullname(key, item)]}"' for item, _ in modules_and_symbols[key][1]]
             )
+        else:
+            items = ([])
 
-            if val != {}:
-                items.extend(dfs(val))
+        if val != {}:
+            items.extend(dfs(val))
 
-            sidebar_items.append(module_sidebar(key, items))
+        sidebar_items.append(module_sidebar(key, items))
 
-        return sidebar_items
+    return sidebar_items
 
-    # Convert class hierarchy into API sidebar
+# Generate article markdown files
+def generate_markdown(article_name: str, symbol_name: str, entity: Any) -> str:
+    """
+    TODO: Method that inputs an object, extracts signature/docstring,
+    and formats as markdown
+    TODO: Method that build index markdown for overview files
+    The overview for the entire API is a special case
+    """
+
+    if symbol_name == "":
+        header = """---
+id: overview
+sidebar_label: "Overview"
+slug: "/api"
+---
+
+:::info
+
+These API stubs are generated from Python via a custom script and will filled
+out in the future.
+
+:::
+
+"""
+        return header
+
+    # Regular modules/functions
+    item = {
+        "id": article_name,
+        "sidebar_label": "Overview" if ismodule(entity) else symbol_name.split(".")[-1],
+        "slug": f"/api/{article_name}",
+        "ref": entity,
+    }
+
+    header = f"""---
+id: {item['id']}
+sidebar_label: {item['sidebar_label']}
+---"""
+
+    # Convert symbol to MDX
+
+    # Imports for custom styling components
+    markdown = ["""import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons'
+import PythonClass from "@theme/PythonClass";
+import PythonFunction from "@theme/PythonFunction";
+import PythonMethod from "@theme/PythonMethod";
+import PythonModule from "@theme/PythonModule";
+import PythonNavbar from "@theme/PythonNavbar";
+"""]
+
+    # Make URL
+    entity_file = entity.__file__ if ismodule(entity) else inspect.getmodule(entity).__file__
+    url = config['settings']['github'] + 'flowtorch/' + entity_file[(len(main_path)+1):].replace('\\', '/')
+    
+    # Make navigation bar
+    markdown.append(f"<PythonNavbar url='{url}'>\n")
+    navigation = []
+    symbol_splits = symbol_name.split('.')
+    for idx in range(len(symbol_splits)):
+        partial_symbol_name = '.'.join(symbol_splits[0:(idx+1)])
+        if idx == len(symbol_splits) - 1:
+            navigation.append(f"*{symbol_splits[idx]}*")
+        elif partial_symbol_name in symbol_to_article:
+            navigation.append(f"[{symbol_splits[idx]}](/api/{symbol_to_article[partial_symbol_name]})")
+        else:
+            navigation.append(f"{symbol_splits[idx]}")
+
+    markdown.append(' <FontAwesomeIcon icon={faAngleDoubleRight} size="sm" /> '.join(navigation))
+    markdown.append("\n</PythonNavbar>\n")
+
+    # Handle known symbol types
+    if isclass(entity):
+        markdown.append(generate_class_markdown( symbol_name, entity))
+        return '\n'.join([header] + markdown)
+
+    elif ismodule(entity):
+        markdown.append(generate_module_markdown( symbol_name, entity))
+        return '\n'.join([header] + markdown)
+
+    # Signature for function
+    elif isfunction(entity):
+        markdown.append(generate_function_markdown( symbol_name, entity))
+        return '\n'.join([header] + markdown)
+
+    # Unknown symbol type
+    else:
+        raise ValueError(f"Symbol {symbol_name} has unknown type {type(symbol_object)}")
+
+if __name__ == "__main__":
+    # Load and validate configuration file
+    import flowtorch
+    config_path = os.path.join(flowtorch.__path__[0], "website/documentation.toml")
+    config = toml.load(config_path)
+    #print(config)
+
+    # Validate module name to document
+    assert 'settings' in config and 'search' in config['settings'] and (type(config['settings']['search']) is str or type(config['settings']['search']) is list)
+
+    # TODO: Try to import module, more validation, etc.
+
+    # Construct regular expressions for includes and excludes
+    # Default include/exclude rules
+    patterns = {
+        'include': {
+            'modules': re.compile(r".+"),
+            'symbols': re.compile(r".+")
+        },
+        'exclude': {
+            'modules': re.compile(r""),
+            'symbols': re.compile(r"")
+        }
+    }
+
+    # Override rules based on configuration file
+    if 'filters' in config:
+        filters = config['filters']
+        for clude, rules in filters.items():
+            for rule, pattern in rules.items():
+                if type(pattern) is list:
+                    pattern = '|'.join(pattern)
+                patterns[clude][rule] = re.compile(pattern)
+
+    # Read in all modules and symbols
+    search = config['settings']['search']
+    search = [search] if type(search) is str else search
+    modules_and_symbols = {}
+    for modname in set(search):
+        modules_and_symbols = {**modules_and_symbols, **walk_packages(modname)} 
+
+    # Apply filtering
+    # TODO: Would be slightly faster if we applied module filtering inside walk_packages
+    tmp = {}
+    for x, y in modules_and_symbols.items():
+        if patterns['include']['modules'].fullmatch(x) is not None and patterns['exclude']['modules'].fullmatch(x) is None:
+
+            new_y1 = [(a, b) for a, b in y[1]
+                if patterns['include']['symbols'].fullmatch(x+'.'+a) is not None and patterns['exclude']['symbols'].fullmatch(x+'.'+a) is None
+            ]
+
+            tmp[x] = (y[0], new_y1)
+
+    modules_and_symbols = tmp
+
+    # Construct list of articles (converting symbols to lower-case and collating)
+    # NOTE: Webservers and Windows machines can't seem to distinguish addresses by
+    # case...
+    articles = {}
+    symbol_to_article = {}
+
+    for mod_name, (module, symbols) in modules_and_symbols.items():
+        if len(symbols):
+            article_name = mod_name.lower()
+            # Find a unique name
+            if article_name in articles:
+                suffix = 1
+                while article_name + str(suffix) in articles:
+                    suffix += 1
+                article_name = article_name + str(suffix)
+
+            articles[article_name] = (mod_name, module)
+            symbol_to_article[mod_name] = article_name
+
+            suffix = 0
+            for symbol_name, symbol in symbols:
+                full_name = mod_name + '.' + symbol_name
+                article_name = full_name.lower()
+
+                # Find a unique name
+                if article_name in articles:
+                    suffix += 1
+                    while article_name + str(suffix) in articles:
+                        suffix += 1
+                    article_name = article_name + str(suffix)
+
+                articles[article_name] = (full_name, symbol)
+                symbol_to_article[full_name] = article_name
+
+    # Generate sidebar
+    # Build hierarchy of modules
+    hierarchy = sparse_module_hierarchy(modules_and_symbols.keys())
+
+    # Create directories if they don't exist
+    main_module = importlib.import_module(search[0])
+    main_path = main_module.__path__[0]
+    sidebar_path = os.path.join(main_path, config['paths']['sidebar'])
+    markdown_path = os.path.join(main_path, config['paths']['markdown'])
+
+    def create_paths(path: str) -> None:
+        try:
+            os.makedirs(path)
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+
+    create_paths(sidebar_path)
+    create_paths(markdown_path)
+
+    # hierarchy: dict of dicts that can be used to build the structure of the API sidebar
+    # modules_and_symbols: a mapping from module names to the module object and the symbol
+    #   object and names in that module
+    # articles: mapping from article name to the symbols in that article
+
+    # We need to iterate over the module hierarchy and work out for each module, which
+    # articles are in it. So, we should construct a mapping from module to articles.
     with open(
-        os.path.join(flowtorch.__path__[0], "../website/api.sidebar.js"), "w"
-    ) as file:
+        os.path.join(os.path.join(main_path, config['paths']['sidebar'], config['paths']['sidebar_filename'])), "w"
+        ) as file:
         print("module.exports = [\n'api/overview',", file=file)
-        print(",".join(dfs(module_hierarchy)), file=file)
+        print(",".join(dfs(hierarchy)), file=file)
         print("];", file=file)
 
-    # Generate markdown files for documentable entities
-    name_entity_mapping = name_entity_mapping.copy()
-    name_entity_mapping[""] = None
-    for name, entity in name_entity_mapping.items():
-        filename, markdown = generate_markdown(name, entity)
+    # For each article, convert the symbols to markdown, etc.
 
-        with open(os.path.join(flowtorch.__path__[0], filename), "w") as file:
-            print(markdown, file=file)
+    # TODO: How to handle when there is a symbol called overview?
+    # Maybe add a key for None instead of "overview"?
+    articles["overview"] = ('', None)
+
+    for article_name, (symbol_name, symbol_object) in articles.items():
+        with open(
+            os.path.join(os.path.join(main_path, config['paths']['markdown'], article_name + '.mdx')), "w"
+        ) as file:
+            print(generate_markdown(article_name, symbol_name, symbol_object), file=file)
+
+"""
+Future: 
+
+* Better solution for when multiple symbols are indistinguishable by case - could read the
+  URL directly with Javascript and display different content accordingly...
+* More thorough testing/debugging
+* Robust exception handling
+* Refactoring into library, script that is a command line utility, if __name__ == "__main__" etc.
+* Verbose option where it outputs which symbols were included/excluded, counts, etc.
+
+"""
