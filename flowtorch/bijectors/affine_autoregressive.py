5ec166a0596c5079ecb2c601356aa95880af39b9
Stefan Webb <info@stefanwebb.me>
2022-05-04 17:13:38 -0700

    Reverted changes to AffineOp since they're in separate PR

--- flowtorch/bijectors/affine_autoregressive.py
+++ flowtorch/bijectors/affine_autoregressive.py
@@ -16,28 +16,15 @@ class AffineAutoregressive(AffineOp, Autoregressive):
         *,
         shape: torch.Size,
         context_shape: Optional[torch.Size] = None,
-        clamp_values: bool = False,
         log_scale_min_clip: float = -5.0,
         log_scale_max_clip: float = 3.0,
         sigmoid_bias: float = 2.0,
-        positive_map: str = "softplus",
-        positive_bias: Optional[float] = None,
     ) -> None:
-        AffineOp.__init__(
-            self,
-            params_fn,
-            shape=shape,
-            context_shape=context_shape,
-            clamp_values=clamp_values,
-            log_scale_min_clip=log_scale_min_clip,
-            log_scale_max_clip=log_scale_max_clip,
-            sigmoid_bias=sigmoid_bias,
-            positive_map=positive_map,
-            positive_bias=positive_bias,
-        )
-        Autoregressive.__init__(
-            self,
+        super().__init__(
             params_fn,
             shape=shape,
             context_shape=context_shape,
         )
+        self.log_scale_min_clip = log_scale_min_clip
+        self.log_scale_max_clip = log_scale_max_clip
+        self.sigmoid_bias = sigmoid_bias
